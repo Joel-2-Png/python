14cb25eae3ce8265cfc37a04228463f080d3d387
MableJoel <mablejoel00001@gmail.com>
2025-08-14 08:59:52 +0000

    premier exercices

--- Exo1.py
+++ Exo1.py
@@ -1,6 +1,6 @@
 nbre = int(input("Entrez un nombre entier : "))
 if nbre /2== 0:
-    print("Le nombre est pair.")    
+    print("Le nombre est pair .")    
 else:
-    print("Le nombre est impair.")      
+    print("Le nombre est impair .")      
     

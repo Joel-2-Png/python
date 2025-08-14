d51e7472e34b485388e8036b67789c1c99746bef
MableJoel <mablejoel00001@gmail.com>
2025-08-14 16:12:29 +0000

    ;

---
+++ Exo7.py
@@ -0,0 +1,7 @@
+personne = {
+    "nom": "Alice",
+    "âge": 30,
+    "ville": "Paris"
+}
+for valeur in personne.values():
+    print(valeur)   
---
+++ Exo8.py
@@ -0,0 +1,6 @@
+personne={
+    "nom": input("Quel est votre nom ? "),
+    "age": int(input("Entrez votre age : ")),
+}
+for infos in personne.values():
+    print(infos)    

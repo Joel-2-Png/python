745a0e3b923f2ed3d24fa141e73e918ad03be553
MableJoel <mablejoel00001@gmail.com>
2025-08-14 09:48:44 +0000

    cinqiéme exercices

---
+++ Exo5.py
@@ -0,0 +1,9 @@
+#nbr=[12, 13, 14, 15, 16]
+#Somme = nbr[0] + nbr[1] + nbr[2] + nbr[3] + nbr[4]
+#print(f"afficher la somme : {Somme} ")
+
+nbr=[12, 13, 14, 15, 16]
+somme=0
+for i in nbr:
+    somme+= i
+print(f"afficher la somme : {somme} ")

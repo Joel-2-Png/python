b6a59db4cf476ce89a2e03a026ec2bb2a9593081
MableJoel <mablejoel00001@gmail.com>
2025-08-21 10:05:34 +0000

    ;

--- Exo12.py
+++ Exo12.py
@@ -1,11 +1,18 @@
+# Définir deux dictionnaires
 dict1 = {
-    "nom": "mable",
-    "prenom": "joel",
-    "age": 18 }
+    "M": 1,
+    "b": 2,
+    "c": 3
+    }
 dict2 = {
-    "nom": "mable",
-    "prenom": "Eyram",
-    "age": 20}
- 
-fusion = {**dict1, **dict2}
+    "c": 30,
+    "d": 4,
+    "e": 5
+    }
+
+# Fusionner les dictionnaires (les clés identiques sont écrasées par dict2)
+fusion = dict1.copy()   # On copie le premier dictionnaire
+fusion.update(dict2)    # On ajoute le deuxième dictionnaire
+
+# Afficher le résultat
 print("Dictionnaire fusionné :", fusion)
